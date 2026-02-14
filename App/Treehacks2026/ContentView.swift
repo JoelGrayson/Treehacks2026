3d23efb1183eb15465fe7118fa4e8d5cd6368152
Joel Grayson <joel@joelgrayson.com>
2026-02-13 23:29:06 -0800

    app initial files

---
+++ App/Treehacks2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Treehacks2026
+//
+//  Created by Joel Grayson on 2/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ App/Treehacks2026/Treehacks2026App.swift
@@ -0,0 +1,17 @@
+//
+//  Treehacks2026App.swift
+//  Treehacks2026
+//
+//  Created by Joel Grayson on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Treehacks2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
